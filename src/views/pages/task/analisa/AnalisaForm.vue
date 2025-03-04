6ccfe884af0336e5294e9ccd0926685f2d61d6b7
ahapsin <ahapsin02@gmail.com>
2025-03-04 10:45:07 +0700

    filter listban

--- src/views/pages/task/analisa/AnalisaForm.vue
+++ src/views/pages/task/analisa/AnalisaForm.vue
@@ -20,6 +20,7 @@
           </n-button>
         </div>
       </div>
+
       <div class="p-4">
         <n-progress
             type="line"
@@ -32,15 +33,18 @@
       </div>
     </div>
     <div class="w-full bg-white rounded-lg p-4">
+      <div class="flex sticky bg-yellow-50 top-0 z-50 justify-end rounded-xl p-4" v-if="bucketParam?.length">
+        <n-button type="success" class="shadow" @click="prevModal = true">Preview</n-button>
+      </div>
       <div class="grid  grid-cols-2 gap-4  p-4">
         <n-form-item label="Permohonan nasabah">
-          <n-select :options="optObject(optParamScoring.permohonan_nasabah_opt)"/>
+          <n-select :options="optObject(optParamScoring.permohonan_nasabah_opt)" placeholder="Silahkan pilih"/>
         </n-form-item>
         <n-form-item label="Status nasabah">
-          <n-select :options="optObject(optParamScoring.status_nasabah_opt)"/>
+          <n-select :options="optObject(optParamScoring.status_nasabah_opt)" placeholder="Silahkan pilih"/>
         </n-form-item>
       </div>
-      <div v-for="(listBucket,i) in bucketParam" :key="listBucket.id">
+      <div v-for="(listBucket,i) in bucketParam" :key="listBucket.key">
         <n-card :title="listBucket.param">
           <template #header-extra>
             <n-button quaternary circle @click="removeParam(i)">
@@ -49,57 +53,124 @@
               </n-icon>
             </n-button>
           </template>
-          <n-form-item v-for="itemParam in listBucket.paramlist">
-            <div class="w-2/6">{{ itemParam.title }}</div>
-            <n-input type="textarea" v-if="itemParam.type == 'single'"/>
-            <div v-else-if="itemParam.type == 'table'">
-              <n-table :bordered="false" :single-line="false" class="w-full" width="100%">
-                <thead>
-                <tr>
-                  <th>tab1</th>
-                  <th>tab2</th>
-                  <th>tab3</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                  <td>col1</td>
-                  <td>col2</td>
-                  <td>col3</td>
-                </tr>
-                </tbody>
-              </n-table>
+          <div v-for="itemParam in listBucket.paramlist" :key="itemParam.key">
+            <n-form-item class="w-full" v-if="itemParam.mode.type == 'area'" :label="itemParam.title">
+              <n-input type="textarea" placeholder="Isi disini"
+                       v-model:value="itemParam.val"/>
+            </n-form-item>
+            <div v-else-if="itemParam.mode.type == 'table'" class="mb-4">
+              <div>
+                <n-data-table :columns="columns" :data="refTable[itemParam.val]" :pagination="pagination"/>
+              </div>
             </div>
-            <n-select v-else/>
-            {{itemParam}}
-          </n-form-item>
+            <n-form-item class="w-full" v-else :label="itemParam.title">
+              <n-select
+                  v-model:value="itemParam.val" placeholder="Silakan pilih"
+                  :options="optObject(itemParam.mode.options)"/>
+            </n-form-item>
+          </div>
         </n-card>
+
       </div>
+
     </div>
   </div>
-  <div class="flex flex-col gap-y-4 pt-4">
-    <!--      <n-card v-for="param in modelArray(useAnalisaModalKerja)" :key="param.id">-->
-    <!--        <div class="flex justify-between">-->
-    <!--          <div class="text-lg font-bold">{{ param.replace("_", " ").toUpperCase() }}</div>-->
-    <!--          <n-switch></n-switch>-->
-    <!--        </div>-->
-    <!--        <div class="bg-slate-50 p-4">-->
-    <!--          asdasd-->
-    <!--        </div>-->
-    <!--      </n-card>-->
-
-  </div>
+  <n-modal v-model:show="prevModal">
+    <n-card class="w-4/6">
+      <n-watermark
+          ref="printAreaRef"
+          cross
+          selectable
+          :font-size="16"
+          :line-height="16"
+          :width="192"
+          :height="128"
+          :x-offset="12"
+          :y-offset="28"
+          :rotate="-10"
+          class="p-4">
+        <div class="flex flex-col gap-y-4">
+          <div class="flex justify-between">
+            <div class="flex items-center gap-2">
+              <img :src="applogo" class="w-[32px] h-[34px]"/>
+              <div class="text-xl font-bold">{{ apptitle }}</div>
+            </div>
+            <div class="w-fit flex-col border items-center flex px-4 border-black">
+              <div class="font-bold">FORM ANALISA KREDIT</div>
+              <div class="text-sm flex justify-end font-mono"><small>NO. 21312312312</small></div>
+            </div>
+          </div>
+          <div>
+            <div class="border border-black p-2">
+              <div class="grid grid-cols-2 gap-2">
+                <div>
+                  <div class="font-bold">
+                    Sdr. Hidayat Rahmat
+                  </div>
+                  <div>
+                    Jalan Sumbawa I/20, Kelurahan Merdeka, Kecamatan Sumurbandung, Bandung 40113
+                  </div>
+                </div>
+                <div>
+                  <table>
+                    <tr>
+                      <td>Produk</td>
+                      <td>:</td>
+                      <td><b>KAMU (Kredit Agunan Multiguna)</b></td>
+                    </tr>
+                    <tr>
+                      <td>Tujuan penggunaan</td>
+                      <td>:</td>
+                      <td><i>Penambahan modal usaha</i></td>
+                    </tr>
+                    <tr>
+                      <td>Plafon</td>
+                      <td>:</td>
+                      <td><i>135.000.000</i></td>
+                    </tr>
+                    <tr>
+                      <td>Jangka Waktu</td>
+                      <td>:</td>
+                      <td><i>32 bulan</i></td>
+                    </tr>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="" v-for="(param,k) in _.sortBy(bucketParam, [function(o) { return o.key; }])" :key="param.key">
+            <div class="text-md font-bold">{{ k + 1 }}. {{ param.param.toUpperCase() }}</div>
+            <div class="grid grid-flow-dense grid-cols-2 gap-2 border border-black p-2">
+              <div v-for="(list,i,n) in param.paramlist" :key="list.id">
+                <div class="px-1">{{ n + 1 }}. {{ list.title }}</div>
+                <div class="border p-1.5 bg-slate-50"><i>{{ list.val }}</i></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </n-watermark>
+      <template #footer>
+        <n-space>
+          <n-button type="info" @click="handlePrintAnalisa">Cetak</n-button>
+        </n-space>
+      </template>
+    </n-card>
+  </n-modal>
 </template>
 <script setup>
 import _ from "lodash";
 import {h} from "vue";
-import {NInput} from "naive-ui";
+import {NInput, NButton, NDatePicker} from "naive-ui";
 
+const apptitle = import.meta.env.VITE_APP_TITLE;
 const applogo = import.meta.env.VITE_APP_LOGO;
 import {useAnalisaModalKerja} from "../../../../models/parameter_analisa.js";
 import {CloseRound as RemoveIcon, AddCircleRound as AddIcon} from "@vicons/material";
 import {useOptAnalisaModalKerja} from "../../../../models/opt_analisa_modal_kerja.js";
+import {useVueToPrint} from "vue-to-print";
+import router from "../../../../router/index.js";
 
+const prevModal = ref(false);
 const paramScoring = reactive(useAnalisaModalKerja);
 const optParamScoring = reactive(useOptAnalisaModalKerja);
 const currentRef = ref(1);
@@ -120,6 +191,7 @@ const findParam = (a, m) => computed(() => {
   return _.includes(a, m);
 })
 const addParam = (e) => {
+  console.log(e);
   bucketParam.value.unshift(e);
 }
 const removeParam = (e) => {
@@ -128,64 +200,84 @@ const removeParam = (e) => {
 const currentStatus = ref("process");
 
 const modelArray = (e) => Object.keys(e).map(key => key);
-
-function createData() {
-  return [
-    {
-      key: 0,
-      name: "John Brown",
-      age: "32",
-      address: "New York No. 1 Lake Park"
-    },
-    {
-      key: 1,
-      name: "Jim Green",
-      age: "42",
-      address: "London No. 1 Lake Park"
-    },
-    {
-      key: 2,
-      name: "Joe Black",
-      age: "32",
-      address: "Sidney No. 1 Lake Park"
-    }
-  ];
-}
-
-const data = ref(createData());
+const tableAnalisaModel = [
+  {
+    bulan: null,
+    mutasi_debet: null,
+    mutasi_kredit: null,
+    saldo_akhir: null,
+  }
+];
+const refTable = reactive({
+  rek_tabungan: [{
+    bulan: null,
+    mutasi_debet: null,
+    mutasi_kredit: null,
+    saldo_akhir: null,
+  }],
+});
 const createColumns = () => [
   {
-    title: "Name",
-    key: "name",
     render(row, index) {
-      return h(NInput, {
-        value: row.name,
+      return h(NButton, {
+        type: !index % 2 ? "primary" : "info",
+        secondary: true,
+        onClick: () => {
+          if (!index % 2) {
+            refTable.rek_tabungan.push(tableAnalisaModel);
+          } else {
+            refTable.rek_tabungan.splice(1);
+          }
+        }
+      }, {
+        default: () => !index % 2 ? "+" : "-",
+      });
+    }
+  },
+  {
+    title: "Bulan",
+    key: "bulan",
+    render(row, index) {
+      return h(NDatePicker, {
+        type: "month",
+        format: "MMyyyy",
+        value: row.bulan,
         onUpdateValue(v) {
-          data.value[index].name = v;
+          refTable.rek_tabungan[index].bulan = v;
         }
       });
     }
   },
   {
-    title: "Age",
-    key: "age",
-    render(row, index) {
-      return h(NInput, {
-        value: row.age,
-        onUpdateValue(v) {
-          data.value[index].age = v;
-        }
-      });
-    }
-  },
-  {
-    title: "Address",
-    key: "address",
+    title: "Mutasi Debet",
+    key: "mutasi_debet",
     render(row, index) {
       return h(NInput, {
         value: row.address,
         onUpdateValue(v) {
-          data.value[index].address = v;
+          refTable.rek_tabungan[index].mutasi_debet = v;
+        }
+      });
+    }
+  }, {
+    title: "Mutasi Kredit",
+    key: "mutasi_kredit",
+    render(row, index) {
+      return h(NInput, {
+        value: row.address,
+        onUpdateValue(v) {
+          refTable.rek_tabungan[index].mutasi_kredit = v;
+        }
+      });
+    }
+  }, {
+    title: "Saldo Akhir",
+    key: "saldo_akhir",
+    render(row, index) {
+      return h(NInput, {
+        value: row.address,
+        onUpdateValue(v) {
+          refTable.rek_tabungan[index].saldo_akhir = v;
         }
       });
     }
@@ -197,9 +289,23 @@ const optObject = (e) => {
     value: v,
   }));
 }
+const columns = createColumns();
+const printAreaRef = ref(null);
+const {handlePrint} = useVueToPrint({
+  content: printAreaRef,
+  documentTitle: "form analisa",
+});
+const handlePrintAnalisa = () => {
+  handlePrint();
+  router.go(-1)
+}
 
-const columns = () => {
-  return createColumns();
+const convertObjectToArray = (obj) => {
+  if (!Array.isArray(obj) || obj.length === 0) {
+    return [];
+  }
+  const keys = Object.keys(obj[0]);
+  return keys.map(key => ({title: key, key: key}));
 }
 
 onMounted(() => {
